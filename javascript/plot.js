ef6e456ddecb37eb0874ca6d433301fb0bf84b68
Pawel <pjurgielewicz@o2.pl>
2017-10-19 17:41:12 +0200

    Struggling with prettifying chart

--- javascript/plot.js
+++ javascript/plot.js
@@ -8,7 +8,12 @@ var contrastingColors = ["#f00", "#0f0", "#00f", "#000", "#0ff", "#f0f", "#f70",
 var contrastingColorIdx = 0;
 
 
-////
+/*
+	DEFINITION OF HOW MANY PARTS EACH <<module>> HAS TO MAKE RELATIVE PLOTS POSSIBLE
+*/
+
+// FOR STRIPS
+
 var stripsPerAPV = 128;
 
 var tidModulesPerDisk = 24 * 2 + 24 * 2 + 40;
@@ -24,7 +29,7 @@ var tecAPVsPerDisk456 =              24 * 2 * 6 + 40 * 4 + 56 * 4 + 40 * 2 * 6 +
 var tecAPVsPerDisk78  =                           40 * 4 + 56 * 4 + 40 * 2 * 6 + 56 * 4 + 80 * 4;
 var tecAPVsPerDisk9   =                                    56 * 4 + 40 * 2 * 6 + 56 * 4 + 80 * 4;
 
-////
+// FOR PIXEL
 
 var rocsInModule = 16;
 var colsInRoc = 52;
@@ -33,55 +38,54 @@ var endcapRing2Modules = 68;
 var endcapDiskModules = endcapRing1Modules + endcapRing2Modules;
 var endcapTotalModules = 672;
 
-// only PIXEL - JUST FOR THE TIME BEING...
 var totalSubModuleCnt = {
 	//https://web.physik.rwth-aachen.de/~klein/eps.pdf
 	//http://abbaneo.web.cern.ch/abbaneo/cms/layout/whole.html
-	"Strip" : [15148, 0, 72784, 72784 * stripsPerAPV], //MODULES, FIBERS, APVS, STRIPS
+	"Strip" : [15148, 72784 / 2, 72784, 72784 * stripsPerAPV], //MODULES, FIBERS, APVS, STRIPS
 
-	"TEC" : [6400, 0, 30208, 30208 * stripsPerAPV],
-	"TIB" : [2724, 0, (4032 + 5184 + 2160 + 2592), (4032 + 5184 + 2160 + 2592) * stripsPerAPV],
-	"TID" : [tidModulesPerDisk * 6, 0, tidAPVsPerDisk * 6, tidAPVsPerDisk * 6 * stripsPerAPV],
-	"TOB" : [5208, 0, (4032 + 4608 + 2592 + 2880 + 4752 + 5328), (4032 + 4608 + 2592 + 2880 + 4752 + 5328) * stripsPerAPV],
+	"TEC" : [6400, 30208 / 2, 30208, 30208 * stripsPerAPV],
+	"TIB" : [2724, 13968 / 2, 13968, 13968 * stripsPerAPV],
+	"TID" : [tidModulesPerDisk * 6, tidAPVsPerDisk * 6 / 2, tidAPVsPerDisk * 6, tidAPVsPerDisk * 6 * stripsPerAPV],
+	"TOB" : [5208, 24192 / 2, 24192, 24192 * stripsPerAPV],
 
-	"TIB Layer 1" : [336 * 2, 0, 4032, 4032 * stripsPerAPV],
-	"TIB Layer 2" : [432 * 2, 0, 5184, 5184 * stripsPerAPV],
-	"TIB Layer 3" : [540, 	  0, 2160, 2160 * stripsPerAPV],
-	"TIB Layer 4" : [648, 	  0, 2592, 2592 * stripsPerAPV],
+	"TIB Layer 1" : [336 * 2, 4032 / 2, 4032, 4032 * stripsPerAPV],
+	"TIB Layer 2" : [432 * 2, 5184 / 2, 5184, 5184 * stripsPerAPV],
+	"TIB Layer 3" : [540, 	  2160 / 2, 2160, 2160 * stripsPerAPV],
+	"TIB Layer 4" : [648, 	  2592 / 2, 2592, 2592 * stripsPerAPV],
 
-	"TID+ Disk 1" : [tidModulesPerDisk, 0, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
-	"TID+ Disk 2" : [tidModulesPerDisk, 0, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
-	"TID+ Disk 3" : [tidModulesPerDisk, 0, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
-	"TID- Disk 1" : [tidModulesPerDisk, 0, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
-	"TID- Disk 2" : [tidModulesPerDisk, 0, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
-	"TID- Disk 3" : [tidModulesPerDisk, 0, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
+	"TID+ Disk 1" : [tidModulesPerDisk, tidAPVsPerDisk / 2, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
+	"TID+ Disk 2" : [tidModulesPerDisk, tidAPVsPerDisk / 2, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
+	"TID+ Disk 3" : [tidModulesPerDisk, tidAPVsPerDisk / 2, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
+	"TID- Disk 1" : [tidModulesPerDisk, tidAPVsPerDisk / 2, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
+	"TID- Disk 2" : [tidModulesPerDisk, tidAPVsPerDisk / 2, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
+	"TID- Disk 3" : [tidModulesPerDisk, tidAPVsPerDisk / 2, tidAPVsPerDisk, tidAPVsPerDisk * stripsPerAPV],
 
-	"TOB Layer 1" : [504 * 2, 0, 4032, 4032 * stripsPerAPV],
-	"TOB Layer 2" : [576 * 2, 0, 4608, 4608 * stripsPerAPV],
-	"TOB Layer 3" : [648, 	  0, 2592, 2592 * stripsPerAPV],
-	"TOB Layer 4" : [720, 	  0, 2880, 2880 * stripsPerAPV],
-	"TOB Layer 5" : [792, 	  0, 4752, 4752 * stripsPerAPV],
-	"TOB Layer 6" : [888, 	  0, 5328, 5328 * stripsPerAPV],
+	"TOB Layer 1" : [504 * 2, 4032 / 2, 4032, 4032 * stripsPerAPV],
+	"TOB Layer 2" : [576 * 2, 4608 / 2, 4608, 4608 * stripsPerAPV],
+	"TOB Layer 3" : [648, 	  2592 / 2, 2592, 2592 * stripsPerAPV],
+	"TOB Layer 4" : [720, 	  2880 / 2, 2880, 2880 * stripsPerAPV],
+	"TOB Layer 5" : [792, 	  4752 / 2, 4752, 4752 * stripsPerAPV],
+	"TOB Layer 6" : [888, 	  5328 / 2, 5328, 5328 * stripsPerAPV],
 
-	"TEC+ Disk 1" : [tecModulesPerDisk123, 0, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
-	"TEC+ Disk 2" : [tecModulesPerDisk123, 0, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
-	"TEC+ Disk 3" : [tecModulesPerDisk123, 0, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
-	"TEC+ Disk 4" : [tecModulesPerDisk456, 0, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
-	"TEC+ Disk 5" : [tecModulesPerDisk456, 0, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
-	"TEC+ Disk 6" : [tecModulesPerDisk456, 0, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
-	"TEC+ Disk 7" : [tecModulesPerDisk78,  0, tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
-	"TEC+ Disk 8" : [tecModulesPerDisk78,  0, tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
-	"TEC+ Disk 9" : [tecModulesPerDisk9,   0, tecAPVsPerDisk9  , tecAPVsPerDisk9   * stripsPerAPV],
+	"TEC+ Disk 1" : [tecModulesPerDisk123, tecAPVsPerDisk123 / 2, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
+	"TEC+ Disk 2" : [tecModulesPerDisk123, tecAPVsPerDisk123 / 2, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
+	"TEC+ Disk 3" : [tecModulesPerDisk123, tecAPVsPerDisk123 / 2, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
+	"TEC+ Disk 4" : [tecModulesPerDisk456, tecAPVsPerDisk456 / 2, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
+	"TEC+ Disk 5" : [tecModulesPerDisk456, tecAPVsPerDisk456 / 2, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
+	"TEC+ Disk 6" : [tecModulesPerDisk456, tecAPVsPerDisk456 / 2, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
+	"TEC+ Disk 7" : [tecModulesPerDisk78,  tecAPVsPerDisk78 / 2,  tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
+	"TEC+ Disk 8" : [tecModulesPerDisk78,  tecAPVsPerDisk78 / 2,  tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
+	"TEC+ Disk 9" : [tecModulesPerDisk9,   tecAPVsPerDisk9 / 2,   tecAPVsPerDisk9  , tecAPVsPerDisk9   * stripsPerAPV],
 
-	"TEC- Disk 1" : [tecModulesPerDisk123, 0, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
-	"TEC- Disk 2" : [tecModulesPerDisk123, 0, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
-	"TEC- Disk 3" : [tecModulesPerDisk123, 0, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
-	"TEC- Disk 4" : [tecModulesPerDisk456, 0, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
-	"TEC- Disk 5" : [tecModulesPerDisk456, 0, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
-	"TEC- Disk 6" : [tecModulesPerDisk456, 0, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
-	"TEC- Disk 7" : [tecModulesPerDisk78,  0, tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
-	"TEC- Disk 8" : [tecModulesPerDisk78,  0, tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
-	"TEC- Disk 9" : [tecModulesPerDisk9,   0, tecAPVsPerDisk9  , tecAPVsPerDisk9   * stripsPerAPV],
+	"TEC- Disk 1" : [tecModulesPerDisk123, tecAPVsPerDisk123 / 2, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
+	"TEC- Disk 2" : [tecModulesPerDisk123, tecAPVsPerDisk123 / 2, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
+	"TEC- Disk 3" : [tecModulesPerDisk123, tecAPVsPerDisk123 / 2, tecAPVsPerDisk123, tecAPVsPerDisk123 * stripsPerAPV],
+	"TEC- Disk 4" : [tecModulesPerDisk456, tecAPVsPerDisk456 / 2, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
+	"TEC- Disk 5" : [tecModulesPerDisk456, tecAPVsPerDisk456 / 2, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
+	"TEC- Disk 6" : [tecModulesPerDisk456, tecAPVsPerDisk456 / 2, tecAPVsPerDisk456, tecAPVsPerDisk456 * stripsPerAPV],
+	"TEC- Disk 7" : [tecModulesPerDisk78,  tecAPVsPerDisk78 / 2,  tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
+	"TEC- Disk 8" : [tecModulesPerDisk78,  tecAPVsPerDisk78 / 2,  tecAPVsPerDisk78 , tecAPVsPerDisk78  * stripsPerAPV],
+	"TEC- Disk 9" : [tecModulesPerDisk9,   tecAPVsPerDisk9 / 2,   tecAPVsPerDisk9  , tecAPVsPerDisk9   * stripsPerAPV],
 
 	////////////////////////////////////////////////////////////////////////////////////////
 
@@ -111,7 +115,7 @@ var totalSubModuleCnt = {
 	"Ring 2 Disk- 2" : [endcapRing2Modules * rocsInModule, endcapRing2Modules * rocsInModule, 0, 0, 0, 0],
 	"Ring 2 Disk- 3" : [endcapRing2Modules * rocsInModule, endcapRing2Modules * rocsInModule, 0, 0, 0, 0],
 
-	// NOISE AND IDCs ARE NOT REPORTED PER RING BUT THE WHOLE RING
+	// NOISE AND IDCs ARE NOT REPORTED PER RING BUT THE WHOLE DISK
 	"Disk 1" : [0, 0, 0, 0, endcapDiskModules * rocsInModule * colsInRoc / 2, endcapDiskModules * rocsInModule * colsInRoc],
 	"Disk 2" : [0, 0, 0, 0, endcapDiskModules * rocsInModule * colsInRoc / 2, endcapDiskModules * rocsInModule * colsInRoc],
 	"Disk 3" : [0, 0, 0, 0, endcapDiskModules * rocsInModule * colsInRoc / 2, endcapDiskModules * rocsInModule * colsInRoc],
@@ -304,7 +308,7 @@ function getChartDataRepresentation(data, is_runByRunOn, is_superimpose, is_rela
 				    	{
 				    		var valueToPush = allVals[i];
 				    		var weight = 0;
-				    		if (/*bxpx != "STR" &&*/ is_relativeValues) // because I do not have dictionary for strip
+				    		if (is_relativeValues) // because I do not have dictionary for strip
 				    		{
 				    			if (totalSubModuleCnt[moduleName][i])
 				    				weight = (totalSubModuleCnt[moduleName][i] * 0.01);
@@ -387,7 +391,7 @@ function getChartDataRepresentation(data, is_runByRunOn, is_superimpose, is_rela
 						label : currentLabel,
 						data : vals[i],
 						borderWidth: 2,
-						borderDash: [5, ((/*bxpx != "STR" &&*/ is_relativeValues) ? 5 : 0)],
+						borderDash: [5, ((is_relativeValues) ? 5 : 0)],
 		            	// borderColor: generateRandomColor(),
 		            	borderColor: getColorFromTable(),
 		            	fill: false,
@@ -432,6 +436,13 @@ function getChartDataRepresentation(data, is_runByRunOn, is_superimpose, is_rela
 function CreatePlot(data, is_runByRunOn, is_superimpose, is_relativeValues, is_linear)
 {
 	chartDataRepresentation = (getChartDataRepresentation(data, is_runByRunOn, is_superimpose, is_relativeValues, null))
+
+	var totalLength = 0;
+	for(var k in data.runInfo)
+	{
+		totalLength += data.runInfo[k].lbs * 100;
+	}
+	console.log("Total length: " + totalLength)
 	
 	if (thisPlotContext == null)
 	{
@@ -454,7 +465,7 @@ function CreatePlot(data, is_runByRunOn, is_superimpose, is_relativeValues, is_l
 					threshold: 100
 				},
 				zoom: {
-					enabled: true,
+					enabled: false,
 					drag: false,
 					mode: 'xy',
 					limits: {
@@ -469,7 +480,8 @@ function CreatePlot(data, is_runByRunOn, is_superimpose, is_relativeValues, is_l
                 },
                 title: {
                 	display : true,
-                	text: "Merged bad Modules for runs: " + chartDataRepresentation.labels[0] + " - " + chartDataRepresentation.labels[chartDataRepresentation.labels.length - 1],
+                	fontSize: 30,
+                	text: "Trends for runs: " + chartDataRepresentation.labels[0] + " - " + chartDataRepresentation.labels[chartDataRepresentation.labels.length - 1],
                 },
 
                 legend: {
@@ -499,6 +511,11 @@ function CreatePlot(data, is_runByRunOn, is_superimpose, is_relativeValues, is_l
 					    // }
 					    
          //        	}
+
+         			labels : {
+         				fontSize: 18,
+         			},
+
                 },
 
                 tooltips: {
@@ -517,11 +534,11 @@ function CreatePlot(data, is_runByRunOn, is_superimpose, is_relativeValues, is_l
 		   //                      },
 		   //                  },
 		   //                  footerFontStyle: 'normal'
-		   			callbacks:{
-		   				title: function(e, d){				// makes tooltip title different than scale label
-		   					return d.labels[e[0].index ];
-		   				}
-		   			},
+		   			// callbacks:{
+		   			// 	title: function(e, d){				// makes tooltip title different than scale label
+		   			// 		return d.labels[e[0].index ];
+		   			// 	}
+		   			// },
 				},
                 scales: {
                     xAxes: [{
@@ -535,19 +552,111 @@ function CreatePlot(data, is_runByRunOn, is_superimpose, is_relativeValues, is_l
                         },
                         ticks:{
                         	userCallback: function(value, index, values){
-                        		return value.split(".")[0];
-                        	}
+                        		var labSpl = value.split(".");
+                        		var run = parseInt(labSpl[0]);
+                        		var lbs = data.runInfo[run].lbs;
+                        		// console.log(lbs);
+
+                        		if (labSpl.length == 1 || 
+                        			(lbs >= totalLength / 10000 && parseInt(labSpl[1]) == Math.ceil(lbs / 2.0)))
+									return labSpl[0];
+								if (labSpl[1] == "00000") return "";
+                        	},
+                        	autoSkip : false,
+                        	maxRotation: 90,
+                        },
+                        offset : true,
+                        gridLines: {
+                        	borderDash: [5, 5],
                         },
                     }],
                     yAxes: getScaleOptions(is_linear, is_relativeValues),
                 },
+
+                // ANNOTATIONS
+
+                annotation: {
+                    events: ['click'],
+                    drawTime: "afterDraw",
+                    annotations: [{
+                        id: 'hline',
+                        type: 'line',
+                        mode: 'horizontal',
+                        scaleID: 'y-axis-0',
+                        value: 0,
+                        endValue: 0,
+                        borderColor: 'rgba(255, 0, 0, 0.5)', // a line without visibility
+                        borderWidth: 5,
+                        label: {
+                            position: 'left',
+                            yAdjust: -20,
+                            xAdjust: 0,
+                            backgroundColor: "red",
+                            content: "5000 LS",
+                            enabled: true
+                        },
+                        onClick: function(e) {
+                            // The annotation is is bound to the `this` variable
+                            console.log('Annotation', e.type, this);
+                        }
+                    },
+
+                    {
+                        type: 'box',
+                        xScaleID: 'x-axis-0',
+                        yScaleID: 'y-axis-0',
+                        xMin: "305045",
+                        xMax: "305237",
+                        yMin: 0.01,
+                        yMax: 0.01,
+                        backgroundColor: 'rgba(101, 33, 171, 0.5)',
+                        borderColor: 'rgb(101, 33, 171)',
+                        borderWidth: 1,
+                    }
+
+                    ]
+                }
 		    }
 		});		
 	}
 	else{
 		console.log("Reusing existing chart...");
 		thisChart.config.data = chartDataRepresentation;
-		thisChart.config.options.title.text = "Merged bad Modules for runs: " + chartDataRepresentation.labels[0].split(".")[0] + " - " + chartDataRepresentation.labels[chartDataRepresentation.labels.length - 1].split(".")[0];
+		thisChart.config.options.title.text = "Trends for runs: " + chartDataRepresentation.labels[0].split(".")[0] + " - " + chartDataRepresentation.labels[chartDataRepresentation.labels.length - 1].split(".")[0];
+
+		thisChart.config.options.scales.xAxes[0].ticks.userCallback = function(value, index, values){
+                        		var labSpl = value.split(".");
+                        		var run = parseInt(labSpl[0]);
+                        		var lbs = data.runInfo[run].lbs;
+                        		// console.log(lbs);
+
+                        		if (labSpl.length == 1 || 
+                        			(lbs >= totalLength / 10000 && parseInt(labSpl[1]) == Math.ceil(lbs / 2.0)))
+									return labSpl[0];
+								if (labSpl[1] == "00000") return "";
+                        		},
+
+      	thisChart.config.options.annotation.annotations.push({
+                        id: 'vline',
+                        type: 'line',
+                        mode: 'vertical',
+                        scaleID: 'x-axis-0',
+                        value: "305188",
+                        borderColor: 'rgba(255, 128, 128, 0.5)', // a line without visibility
+                        borderWidth: 5,
+                        label: {
+                            position: 'left',
+                            yAdjust: -20,
+                            xAdjust: 0,
+                            backgroundColor: "red",
+                            content: "FILL",
+                            enabled: true
+                        },
+                        onClick: function(e) {
+                            // The annotation is is bound to the `this` variable
+                            console.log('Annotation', e.type, this);
+                        }
+                    });
 
 		thisChart.resetZoom();
 		thisChart.update();	
